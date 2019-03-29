423900a53441c57e069eea59495d00d8a71087d3
tleveque <thierry.leveque@askida.com>
2019-03-29 08:30:48 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'editorAddLineBug';
+export class AppComponent implements OnInit{
+  editorText: string;
+  editMode: boolean = false;
+  textToCopy: string;
+
+  public ngOnInit(): void {
+
+    this.textToCopy = '<p>Title:</p><ol><li>line 1</li><li>Line 2</li><li>Line 3</li></ol>';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditorModule } from 'primeng/editor';
+import { InputTextModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ButtonModule,
+    EditorModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
